e392a281e19d4e15cbe04cf9789956e6baef6992
Phangg <leeckdwns9305@gmail.com>
2024-09-02 22:11:25 +0900

    🏫 Feat: 세팅 뷰 일부 구현
    viewmodel 분리 및 세부작업 필요

---
+++ Projects/Feature/SettingFeature/Sources/Model/Setting.swift
@@ -0,0 +1,45 @@
+//
+//  Setting.swift
+//  SettingFeature
+//
+//  Created by phang on 9/2/24.
+//  Copyright © 2024 Sentir. All rights reserved.
+//
+
+// MARK: - 세팅 아이템의 우측 아이콘 타입
+public enum SettingType {
+    case navigate
+    case toggle
+    case text
+}
+
+// MARK: - 세팅 아이템 모델
+public enum SettingModel: String, CaseIterable {
+    //
+    case lock = "잠금 설정"
+    case notice = "알림 설정"
+    case displayMode = "다크모드 / 라이트 모드"
+    case font = "폰트 설정"
+    //
+    case deleteData = "데이터 삭제"
+    case backUpData = "데이터 백업"
+    case restoreData = "데이터 복원"
+    //
+    case donation = "후원하기"
+    case feedback = "의견 및 피드백"
+    //
+    case appVersion = "앱 버전"
+    case openSource = "오픈 소스"
+    
+    //
+    var type: SettingType {
+        switch self {
+        case .lock, .notice:
+            return .toggle
+        case .deleteData, .backUpData, .restoreData, .donation, .feedback, .displayMode, .font, .openSource:
+            return .navigate
+        case .appVersion:
+            return .text
+        }
+    }
+}
--- Projects/Feature/SettingFeature/Sources/Presentation/View/SettingView.swift
+++ Projects/Feature/SettingFeature/Sources/Presentation/View/SettingView.swift
@@ -7,12 +7,81 @@
 //
 
 import SwiftUI
+import DesignSystem
 
 public struct SettingView: View {
+    @State private var toggleStates: [SettingModel: Bool] = [:]
+
     public init() { }
-    
+
     public var body: some View {
-        Text("Setting View")
+        NavigationStack {
+            //
+            ScrollView {
+                LazyVStack(spacing: 10) {
+                    ForEach(SettingModel.allCases, id: \.self) { item in
+                        //
+                        HStack(alignment: .center) {
+                            //
+                            Text(item.rawValue)
+                                .font(.callout)
+                                .fontWeight(.regular)
+                                .padding(.vertical, 10)
+                            //
+                            Spacer()
+                            //
+                            settingItemIcon(item)
+                        }
+                        //
+                        if item == .font || item == .restoreData || item == .feedback || item == .openSource {
+                            Rectangle()
+                                .fill(.clear)
+                                .frame(height: 20)
+                        } else {
+                            Divider()
+                        }
+                    }
+                }
+                .padding(20)
+            }
+            .safeAreaPadding(.bottom, 40)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar { settingViewToolbarContent() }
+        }
+        .tint(DesignSystemAsset.customBlack.swiftUIColor)
+    }
+    
+    @ViewBuilder
+    private func settingItemIcon(_ item: SettingModel) -> some View {
+        switch item.type {
+        case .navigate:
+            Button {
+                //
+            } label: {
+                Image(systemName: "chevron.right")
+                    .font(.callout)
+            }
+        case .toggle:
+            Toggle("", isOn: Binding(
+                get: { toggleStates[item] ?? false },
+                set: { toggleStates[item] = $0 }
+            ))
+            .labelsHidden()
+        case .text:
+            // TODO: -
+            Text("1.0.0")
+                .font(.callout)
+        }
+    }
+    
+    @ToolbarContentBuilder
+    private func settingViewToolbarContent() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            Text("설정")
+                .font(.title2)
+                .padding(20)
+        }
     }
 }
 
