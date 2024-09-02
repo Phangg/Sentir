03ac279fd9315bcebd455d6aee8ae1ec6ad4446f
Phangg <leeckdwns9305@gmail.com>
2024-09-02 20:26:52 +0900

    ✨ Add: splash 화면 파일 생성

---
+++ Projects/MainTabFeature/Sources/Presentation/Splash/SplashView.swift
@@ -0,0 +1,21 @@
+//
+//  SplashView.swift
+//  MainTabFeature
+//
+//  Created by phang on 9/2/24.
+//  Copyright © 2024 Sentir. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SplashView: View {
+    public init() { }
+    
+    public var body: some View {
+        Text("Splash View")
+    }
+}
+
+#Preview {
+    SplashView()
+}
