679076d2457c9ba3badf0b2d872de971642505dc
Phangg <leeckdwns9305@gmail.com>
2024-09-11 00:35:51 +0900

    ✏️ Feat: JournalView 기본 화면 틀 구성

--- Projects/Feature/JournalFeature/Sources/Presentation/View/JournalView.swift
+++ Projects/Feature/JournalFeature/Sources/Presentation/View/JournalView.swift
@@ -7,12 +7,73 @@
 //
 
 import SwiftUI
+import DesignSystem
 
 public struct JournalView: View {
+    @State private var viewState: JournalViewState = .list
+    
     public init() { }
 
     public var body: some View {
-        Text("Journal View")
+        NavigationStack {
+            //
+            VStack {
+                //
+                switch viewState {
+                case .list:
+                    //
+                    ListView()
+                        .transition(.opacity)
+                case .calendar:
+                    //
+                    CalendarView()
+                        .transition(.opacity)
+                }
+            }
+            .animation(.easeInOut, value: viewState)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar { journalViewToolbarContent() }
+        }
+        //
+        .tint(DesignSystemAsset.black)
+    }
+    
+    @ToolbarContentBuilder
+    private func journalViewToolbarContent() -> some ToolbarContent{
+        ToolbarItem(placement: .topBarLeading) {
+            //
+            Text("기록")
+                .textStyle(Title())
+                .padding(.horizontal, 10)
+        }
+        //
+        if viewState == .list {
+            ToolbarItem(placement: .topBarTrailing) {
+                //
+                Button {
+                    // TODO: -
+                    print("검색")
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .tint(DesignSystemAsset.black)
+                        .padding(.leading, 10)
+                }
+            }
+        }
+        ToolbarItem(placement: .topBarTrailing) {
+            //
+            Button {
+                // TODO: -
+                withAnimation {
+                    viewState = viewState == .list ? .calendar : .list
+                }
+            } label: {
+                Image(systemName: viewState == .list ? "calendar": "list.dash")
+                    .tint(DesignSystemAsset.black)
+                    .padding(.trailing, 10)
+            }
+        }
     }
 }
 
